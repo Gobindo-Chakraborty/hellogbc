3c3e3705f1279b301159904f650e6a7996ce1aa2
Gobindo Chakraborty <gobindochakraborty108@gmail.com>
2026-07-18 15:54:48 +0600

    project heading and description added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/pages/home/Hero";
+import Projects from "@/components/pages/home/Projects";
 import WIP from "@/components/ui/WIP";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Projects />
       <WIP />
     </>
   );
---
+++ components/pages/home/Projects.tsx
@@ -0,0 +1,19 @@
+export default function Projects() {
+  return (
+    <section className="mt-31.25 w-10/12 xl:max-w-286.5 mx-auto">
+      <h1 className="font-bold text-heading-1 text-center text-[2rem] mb-15">
+        Real-World Projects
+      </h1>
+      <div className="bg-secondary-bg-dark-1 rounded-[10px] border border-secondary-bg-dark-1">
+        <p className="text-secondary-text-light-2 before:content-[''] before:absolute before:bg-live-view before:w-0.5 before:h-20.5  py-8 pl-21.5 pr-13.75 before:-ml-7.5">
+          I may not have worked at a company yet, but I’ve built real-world
+          projects and continuously improve my skills through hands-on
+          development. I focus on learning by building that helped me improve my
+          frontend architecture, responsiveness, UI design, and development
+          workflow.
+        </p>
+      </div>
+      <div></div>
+    </section>
+  );
+}
