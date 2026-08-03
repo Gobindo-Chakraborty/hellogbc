4de8d165c0281b277cd6c6c36115aa60fa860fbb
Gobindo Chakraborty <gobindochakraborty108@gmail.com>
2026-08-03 18:26:33 +0600

    Formspree tested successfully

--- components/pages/contact/ContactForm.tsx
+++ components/pages/contact/ContactForm.tsx
@@ -9,55 +9,57 @@ export default function ContactForm() {
         </h1>
 
         <form
-          action=""
-          className="border-secondary-bg-dark-1 rounded-[30px] border pt-6 pr-5.5 pb-6.5 pl-5.75"
+          action="https://formspree.io/f/mrennoqj"
+          method="POST"
+          className="border-secondary-bg-dark-1 mx-auto max-w-241.5 rounded-[30px] border pt-6 pr-5.5 pb-6.5 pl-5.75"
         >
           <p className="text-center">
             Have an idea, project, or opportunity? I’d love to hear from you.
             Let’s create something meaningful together.
           </p>
+          <div className="mx-auto max-w-142.5">
+            <div className="mt-9.25 flex flex-col items-start gap-2.5">
+              <label htmlFor="name" className="">
+                Your Name
+              </label>
+              <input
+                className="border-secondary-bg-dark-1 bg-secondary-bg h-10 w-full rounded-[10px] border"
+                type="text"
+                name="name"
+                id="name"
+                required
+              />
+            </div>
 
-          <div className="mt-9.25 flex flex-col items-start gap-2.5">
-            <label htmlFor="name" className="">
-              Your Name
-            </label>
-            <input
-              className="border-secondary-bg-dark-1 bg-secondary-bg h-10 w-full rounded-[10px] border"
-              type="text"
-              name="name"
-              id="name"
-              required
-            />
+            <div className="mt-5 flex flex-col items-start gap-2.5">
+              <label htmlFor="email">Email Address</label>
+              <input
+                className="border-secondary-bg-dark-1 bg-secondary-bg h-10 w-full rounded-[10px] border"
+                type="email"
+                name="email"
+                id="email"
+                required
+              />
+            </div>
+
+            <div className="mt-5 flex flex-col items-start gap-2.5">
+              <label htmlFor="message">Message</label>
+              <textarea
+                className="border-secondary-bg-dark-1 bg-secondary-bg w-full rounded-[10px] border"
+                name="message"
+                id="message"
+                rows={6}
+                required
+              />
+            </div>
+
+            <button
+              className="bg-secondary-bg-dark-1 text-completion-status mt-7.5 w-full rounded-[10px] pt-3 pb-2.75 text-sm leading-[100%] font-semibold"
+              type="submit"
+            >
+              Send
+            </button>
           </div>
-
-          <div className="mt-5 flex flex-col items-start gap-2.5">
-            <label htmlFor="email">Email Address</label>
-            <input
-              className="border-secondary-bg-dark-1 bg-secondary-bg h-10 w-full rounded-[10px] border"
-              type="email"
-              name="email"
-              id="email"
-              required
-            />
-          </div>
-
-          <div className="mt-5 flex flex-col items-start gap-2.5">
-            <label htmlFor="message">Message</label>
-            <textarea
-              className="border-secondary-bg-dark-1 bg-secondary-bg w-full rounded-[10px] border"
-              name="message"
-              id="message"
-              rows={6}
-              required
-            />
-          </div>
-
-          <button
-            className="bg-secondary-bg-dark-1 text-completion-status mt-7.5 w-full rounded-[10px] pt-3 pb-2.75 text-sm leading-[100%] font-semibold"
-            type="submit"
-          >
-            Send
-          </button>
         </form>
       </div>
 
